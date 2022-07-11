aae3ef95011ccf322122fcb3b615deb4c0200508
manoloacademia <luispablosegovia@icloud.com>
2022-07-11 09:27:12 -0300

    This is the opening commit from the windows laptop.

--- blog_practice/blog/urls.py
+++ blog_practice/blog/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('pages/', views.pages, name='pages'),
 ]
--- blog_practice/blog/views.py
+++ blog_practice/blog/views.py
@@ -5,6 +5,12 @@ from .models import Post
 
 # Create your views here.
 def home(request):
+    return render(request, 'home.html', {})
+
+def about(request):
+    return HttpResponse('This is the about page, from myself.')
+
+def pages(request):
     text_list = Post.objects.all()
     context = {'text_list': text_list}
-    return render(request, 'home.html', context)
+    return render(request, 'pages.html', context)
